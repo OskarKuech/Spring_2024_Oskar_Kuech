7803d1b3b82c1d2662440ff03fdbffbc1554ff7d
Oskar Kuech <oskue@gmx.de>
2024-03-14 18:17:30 +0100

    Dynamic Detail-View.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express, { request, response } from 'express';
+import fs from 'fs';
 
 
 const app = express();
@@ -8,21 +9,35 @@ const PORT = 3000;
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }))
 
+const details = JSON.parse(fs.readFileSync('./views/movies/details.json'));
+
 
 app.get('/', (request, response) => {
     response.render('index')
 })
 
-
 app.get('/:slug', (request, response) => {
     response.render(request.params.slug)
-    response.send('This Page does not exist.')
+    response.send('This Page does not exist. HAHA')
 })
 
-app.get('/movies/:movieSlug', (request, response) => {
-    const movieSlug = request.params.movieSlug;
-    response.render('movies/' + movieSlug);
-    response.send(`${movieSlug} does not exist in the Databse (because there is no...).`)
+app.get('/movies/:slug', (request, response) => {
+    const movieSlug = request.params.slug.toLowerCase();
+    const movieDetails = details.find(movie => movie.slug.toLowerCase().replace(/\s+/g, '-') === movieSlug);
+    if (movieDetails) {
+        response.render('./includes/movie-detail-view.ejs', { 
+            title: movieDetails.title,
+            release_date: movieDetails.release_date,
+            runtime: movieDetails.runtime,
+            director: movieDetails.director,
+            genre: movieDetails.genre,
+            description: movieDetails.description,
+            poster: movieDetails.poster,
+            trailer_id: movieDetails.trailer_id
+        });
+    } else {
+        response.render('error');
+    }
 });
 
 
