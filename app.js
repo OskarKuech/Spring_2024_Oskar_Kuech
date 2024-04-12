0a3b54ef82de54bd6a0d99e7e06e8de27b6fa13d
Oskar Kuech <oskue@gmx.de>
2024-04-12 15:38:32 +0200

    Final-Hand In

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ import morgan from 'morgan';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import favicon from 'serve-favicon';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -77,43 +80,13 @@ const moviesSchema = new mongoose.Schema({
     }
   })
 
-
-
-  // app.post('/movies', async (request, response) => {
-  //   console.log('Received request to create user:', request.body);
-  //   try {
-  //       const movie = new Movie({
-  //         slug: 'oppenheimer',
-  //         title: 'Oppenheimer',
-  //         director: 'Christopher Nolan',
-  //         runtime: '3h 1m',
-  //         release_date: 'Jul 20th 2023',
-  //         genre: 'Drame, Historie',
-  //         description: "The story of J. Robert Oppenheimer's role in the development of the atomic bomb during World War II.",
-  //         poster:'https://image.tmdb.org/t/p/original/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg',
-  //         trailer_id: 'NjlA8pKUavY'
-  //       })
-  //       await movie.save()
-        
-  //       response.send('Movie Created')
-  //   }catch (error) {
-  //       console.error(error)
-  //       response.send('Error: Movie could not be created.')
-  //   }
-  // })
-
-
-
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
-//const details = JSON.parse(fs.readFileSync('./views/movies/details.json'));
-
-mongoose.connect('mongodb://127.0.0.1:27017/filmstarts')
-//mongoose.connect('mongodb+srv://oskue:6$e7UErJa.xMZNp@cluster0.uzhjuzp.mongodb.net/filmstarts?retryWrites=true&w=majority&appName=Cluster0') //I know that this is not perfect because you normally wouldnt want the your password to be visible in the file but I have no Idea how to change this and really had no fun with databases at all!
-    .then(() => console.log('Database is connected'))
-    .catch (error => console.error(error))
+mongoose.connect(process.env.DB_CONNECTION_STRING)
+  .then(() => console.log('Database is connected'))
+  .catch(error => console.error(error));
 
 
 app.get('/', (request, response) => {
@@ -121,98 +94,87 @@ app.get('/', (request, response) => {
 })
 
 app.get('/:slug', (request, response) => {
-    response.render(request.params.slug)
-    // response.send('This Page does not exist.')
-})
-
-// app.get('/movies/:slug', (request, response) => {
-//     const movieSlug = request.params.slug
-//     const movieDetails = details.find(movie => movie.slug.toLowerCase().replace(/\s+/g, '-') === movieSlug);
-//     if (movieDetails) {
-//         response.render('./includes/movie-detail-view.ejs', { 
-//             title: movieDetails.title,
-//             release_date: movieDetails.release_date,
-//             runtime: movieDetails.runtime,
-//             director: movieDetails.director,
-//             genre: movieDetails.genre,
-//             description: movieDetails.description,
-//             poster: movieDetails.poster,
-//             trailer_id: movieDetails.trailer_id
-//         });
-//     } else {
-//         response.render('error');
-//     }
-// });
-
-
+  try {
+      response.render(request.params.slug, (err, html) => {
+          if (err) {
+              console.error(err);
+              response.status(404).render('error');
+          } else {
+              response.send(html);
+          }
+      });
+  } catch (error) {
+      console.error(error);
+      response.status(500).render('error');
+  }
+});
 
 const userSchema = new mongoose.Schema({
-    name:       { type: String, required: true },
-    email:      { type: String, unique: true, required: true },
-    password:   { type: String, required: true }, 
-  })
-  
-  const User = mongoose.model('User', userSchema)
+  name:       { type: String, required: true },
+  email:      { type: String, unique: true, required: true },
+  password:   { type: String, required: true }, 
+})
+
+const User = mongoose.model('User', userSchema)
 
 
 app.get('/sign-up', (request, response) => {
-    response.render('sign-up')
-  })
+  response.render('sign-up')
+})
 
 
 app.post('/users', async (request, response) => {
-    try {
-      const user = new User({
-        name: request.body.name,
-        email: request.body.email,
-        password: request.body.password
-      })
-      await user.save()
-  
-      //response.send('User successfully Created')
-      response.redirect('/user')
-    }catch (error) {
-      console.error(error)
-      response.send('Error: The User could not be created.')
-    }
-  })
+  try {
+    const user = new User({
+      name: request.body.name,
+      email: request.body.email,
+      password: request.body.password
+    })
+    await user.save()
 
-  app.post('/users/:id/delete', async (request, response) => {
-    try {
-      const userId = request.params.id;
-      await User.deleteOne({ _id: userId });
-      response.redirect('/user');
-    } catch (error) {
-      console.error(error);
-      response.send('Error: The User could not be deleted.');
-    }
-  });
+    response.redirect('/user')
+  }catch (error) {
+    console.error(error)
+    response.send('Error: The User could not be created.')
+  }
+})
 
-  app.get('/users/:id/edit', async (request, response) => {
-    try {
-      const user = await User.findById(request.params.id).exec();
-      if (!user) throw new Error('User not found');
-      response.render('edit-user', { user: user });
-    } catch (error) {
-      console.error(error);
-      response.render('error', { message: 'Error retrieving user for edit' });
-    }
-  });
+app.post('/users/:id/delete', async (request, response) => {
+  try {
+    const userId = request.params.id;
+    await User.deleteOne({ _id: userId });
+    response.redirect('/user');
+  } catch (error) {
+    console.error(error);
+    response.send('Error: The User could not be deleted.');
+  }
+});
 
-  app.post('/users/:id/update', async (request, response) => {
-    try {
-      const userId = request.params.id;
-      const updatedData = {
-        name: request.body.name,
-        email: request.body.email,
-      };
-      await User.findByIdAndUpdate(userId, updatedData, { new: true });
-      response.redirect('/user');
-    } catch (error) {
-      console.error(error);
-      response.send('Error: The User could not be updated.');
-    }
-  });
+app.get('/users/:id/edit', async (request, response) => {
+  try {
+    const user = await User.findById(request.params.id).exec();
+    if (!user) throw new Error('User not found');
+    response.render('edit-user', { user: user });
+  } catch (error) {
+    console.error(error);
+    response.render('error', { message: 'Error retrieving user for edit' });
+  }
+});
+
+app.post('/users/:id/update', async (request, response) => {
+  try {
+    const userId = request.params.id;
+    const updatedData = {
+      name: request.body.name,
+      email: request.body.email,
+    };
+    await User.findByIdAndUpdate(userId, updatedData, { new: true });
+    response.redirect('/user');
+  } catch (error) {
+    console.error(error);
+    response.send('Error: The User could not be updated.');
+  }
+});
 
 
 app.listen(PORT, () => {
